c24e19d87b343c92cdcea3c097b0254c5196d925
Sean McShane <mcshanes@duq.edu>
2017-04-20 21:08:41 -0400

    Added JS function to insert PG variable inits

---
+++ htdocs/duq/js/InsertIntoPG.js
@@ -0,0 +1,10 @@
+function insertInits(pgString, initArr){
+	for(var i=0; i < initArr.length; i++){
+		initArr[i] = initArr[i] + " ";
+		if(i == initArr.length-1){
+			initArr[i] = initArr[i] + '<br>';
+		}
+		pgString = splitAndInsert(pgString, findIndex('TEXT(beginproblem());', pgString, 'before'), pgString.length, '<br>' + initArr[i]);
+	}
+	return pgString;
+} 
